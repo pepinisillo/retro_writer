b4957a9ba70cc40a8fef45232ba0391cd8a88583
pepinisillo <l22211664@tectijuana.edu.mx>
2026-06-05 13:52:49 -0700

    fix problema de rutas e inicio con 256 colores en xterm

--- desktop/run_kitty.sh
+++ desktop/run_kitty.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 
-exec kitty --working-directory "$DIR" "$DIR/build/retro_writer_tv" "$@"
+exec kitty --working-directory "$DIR" "$DIR/build/retro_writer_tv" "$DIR" "$@"
--- desktop/run_xterm_sixel.sh
+++ desktop/run_xterm_sixel.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
-DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 
-exec xterm -ti vt340 -fn fixed -e env RETRO_WRITER_FORCE_SIXEL=1 "$DIR/build/retro_writer_tv" "$@"
+exec xterm -ti vt340 -tn xterm-256color -fn fixed \
+  -e env TERM=xterm-256color COLORTERM=truecolor RETRO_WRITER_FORCE_SIXEL=1 \
+  "$DIR/build/retro_writer_tv" "$DIR" "$@"
