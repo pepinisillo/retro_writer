928db890890eeff76b7cd1b264d582d3be59a466
pepinisillo <l22211664@tectijuana.edu.mx>
2026-06-05 14:49:45 -0700

    quitar forceo de tamano en xterm

--- desktop/run_xterm_sixel.sh
+++ desktop/run_xterm_sixel.sh
@@ -3,6 +3,6 @@ set -eu
 
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 
-exec xterm -ti vt340 -tn xterm-256color -fn fixed \
+exec xterm -ti vt340 -tn xterm-256color \
   -e env TERM=xterm-256color COLORTERM=truecolor RETRO_WRITER_FORCE_SIXEL=1 \
   "$DIR/build/retro_writer_tv" "$DIR" "$@"
